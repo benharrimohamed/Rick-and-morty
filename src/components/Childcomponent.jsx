d005057569822eba9f0d16233656de56e51594e0
MOHAMMED BEN HARRI <benharri0mohammed@gmail.com>
2020-09-01 16:22:38 +0100

    Add more styling to the app

---
+++ src/components/Childcomponent.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 300,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    title : {
+      textAlign : 'left'
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
+
+export default function Childcomponent (props)  {
+    
+        const classes = useStyles();
+         
+        return (
+
+     <Grid item xs={3}>
+      <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {props.name.charAt(0)}
+          </Avatar>
+        }
+        title={props.name}
+        subheader={props.status}
+        className={classes.title}
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.image}
+        title="Paella dish"
+      />
+      <CardContent>
+      <Typography variant="caption" display="block" gutterBottom className={classes.title}>
+        Species : {props.species}
+      </Typography>
+      <Typography variant="caption" display="block" gutterBottom className={classes.title}>
+        Gender : {props.gender}
+      </Typography>
+      <Typography variant="caption" display="block" gutterBottom className={classes.title}>
+        Origin : {props.origin}
+      </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+    </Grid>
+        );
+    }
--- src/components/Mycomponent.jsx
+++ src/components/Mycomponent.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Childcomponent from './Childcomponent'
+import Appbar from './Appbar'
+import axios from 'axios'
+import { Container } from '@material-ui/core'
+
+
 
 
 export default class Mycomponent extends React.Component {
@@ -27,14 +22,13 @@ export default class Mycomponent extends React.Component {
 
     componentDidMount()
     {
-        fetch('https://rickandmortyapi.com/api/character/')
-        .then(res => res.json())
+        axios.get('https://rickandmortyapi.com/api/character/')
         .then(res => {
+          
             this.setState({
-                items : res.results,
+                items : res.data.results,
                 isLoaded : true
             })
-            console.log(res.results);
         }).catch (err => {
             console.log(err)
         })
@@ -47,7 +41,10 @@ export default class Mycomponent extends React.Component {
          return <div>Loading ...</div>
          else
          {return (
-            <Grid container lg={12} xs={12} sm={12} spacing={3}>
+            <div>
+            <Container maxWidth="lg" spacing={2}> 
+            <Appbar></Appbar>
+            <Grid container spacing={2}>
              { items.map (item => 
              <Childcomponent 
              key={item.name} 
@@ -55,54 +52,17 @@ export default class Mycomponent extends React.Component {
              status ={item.status} 
              species={item.species}
              image={item.image}
+             gender={item.gender}
+             origin={item.origin.name}
              ></Childcomponent>)}
             </Grid>
+            </Container> 
+            </div>
          );}
     }
 }
 
-class Childcomponent extends React.Component {
 
-    render () {
-        return (
-
-     <Grid item xs={4}>
-      <Card>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" src={this.props.image}>
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={this.props.name}
-        subheader={this.props.status}
-      />
-      <CardMedia
-        image={this.props.image}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-    </Grid>
-        );
-    }
-}
 
 
     
