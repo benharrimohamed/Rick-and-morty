57008f6bca9825e67d5dabe80156a04bf91318ee
MOHAMMED BEN HARRI <benharri0mohammed@gmail.com>
2020-09-04 23:59:32 +0100

    Adding avatar badge

--- src/components/Childcomponent.jsx
+++ src/components/Childcomponent.jsx
@@ -1,49 +1,58 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles,withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import Badge from '@material-ui/core/Badge';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 
 
-const useStyles = makeStyles((theme) => ({
+export default function Childcomponent (props)  {
+
+  const StyledBadge = withStyles((theme) => ({
+    badge: {
+      backgroundColor: props.status == "Alive" ? "#5cb85c" : "#d9534f",
+      boxShadow: "0 0 0 3px #fff"
+    },
+  }))(Badge);
+  
+  
+  const useStyles = makeStyles((theme) => ({
     root: {
-      maxWidth: 300,
     },
     media: {
       height: 0,
-      paddingTop: '56.25%', // 16:9
+      paddingTop: '56.25%', 
     },
     title : {
       textAlign : 'left'
     },
     avatar: {
-      backgroundColor: "#fc7703",
+      backgroundColor: "#0275d8",
     },
   }));
-
-export default function Childcomponent (props)  {
     
-        const classes = useStyles();
+  const classes = useStyles();
          
-        return (
+    return (
 
-     <Grid item xs={3}>
+     <Grid item md={3} xs={12} sm={6}>
       <Card className={classes.root}>
       <CardHeader
         avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            {props.name.charAt(0)}
-          </Avatar>
+          <StyledBadge
+          overlap="circle"
+          anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+          }}
+          variant="dot"
+        >
+        <Avatar alt="Remy Sharp">{props.name.charAt(0)}</Avatar>
+      </StyledBadge>
         }
         title={props.name}
         className={classes.title}
@@ -64,14 +73,6 @@ export default function Childcomponent (props)  {
         Origin : {props.origin}
       </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
     </Card>
     </Grid>
         );
