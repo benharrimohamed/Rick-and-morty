14656b028dcaf4655627f8ba764482d46dd332b9
MOHAMMED BEN HARRI <benharri0mohammed@gmail.com>
2020-09-05 00:30:02 +0100

    Updates

---
+++ precache-manifest.8e17bc7d11c20bcc7f0e8121b3a5d6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f021eb35cbb32cd274b39f73b13279d0",
+    "url": "/Rick-and-morty/index.html"
+  },
+  {
+    "revision": "106dfcd252d46dd92752",
+    "url": "/Rick-and-morty/static/css/main.af13c50f.chunk.css"
+  },
+  {
+    "revision": "1c9a31ca24304bae90d6",
+    "url": "/Rick-and-morty/static/js/2.58c57769.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Rick-and-morty/static/js/2.58c57769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106dfcd252d46dd92752",
+    "url": "/Rick-and-morty/static/js/main.5330fdc3.chunk.js"
+  },
+  {
+    "revision": "7de1099ddd7ee700e463",
+    "url": "/Rick-and-morty/static/js/runtime-main.b98d0bb3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Rick-and-morty/precache-manifest.8e17bc7d11c20bcc7f0e8121b3a5d6ee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rick-and-morty/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
